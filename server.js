9626223c326d8e4fb30c7b5d5915303a1bfa0fee
Milan Petrovic <milanmushin@gmail.com>
2022-02-14 23:31:42 +0100

    Form submit and log input value for test

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
-app.use(express.static('public'))
+app.use(express.urlencoded({extended: false}));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.post('/create-item', (req, res) => {
+    console.log(req.body.task);
+    res.send('Thanks for submitting the form');
 });
 
 app.listen(3000);
